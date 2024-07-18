8faebdd1f7ef3e8924498541a5f8ca44f49184ee
VietDuc3004 <dangvietduc3004@gmail.com>
2024-07-18 10:00:38 +0700

    no message

--- Manstore/src/main/java/com/example/manstore/controller/DotGiamGiaController.java
+++ Manstore/src/main/java/com/example/manstore/controller/DotGiamGiaController.java
@@ -6,19 +6,12 @@ import com.example.manstore.service.DotGiamGiaService;
 import com.example.manstore.service.HoaDonService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.time.LocalDate;
-import java.time.chrono.ChronoLocalDate;
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class DotGiamGiaController {
@@ -39,7 +32,7 @@ public class DotGiamGiaController {
 
     @RequestMapping("/admin/add-promotion")
     public String addPromotion(Model model, @RequestParam(value = "id", required = false) Integer id) {
-            model.addAttribute("promotion", new DotGiamGia());
+        model.addAttribute("promotion", new DotGiamGia());
         return "admin/promotion/add-promotion";
     }
 
@@ -74,11 +67,10 @@ public class DotGiamGiaController {
             model.addAttribute("errorName", "Tên đợt giảm giá quá dài!");
         }
 
-       if (promotion.getNgayBatDau() == null) {
+        if (promotion.getNgayBatDau() == null) {
             isValid = true;
             model.addAttribute("errorNgayBatDau", "Ngày bắt đầu chưa được chọn!");
-        }
-       else if (promotion.getNgayBatDau().isBefore(LocalDate.now())) {
+        } else if (promotion.getNgayBatDau().isBefore(LocalDate.now())) {
             isValid = true;
             model.addAttribute("errorNgayBatDau", "Ngày bắt đầu không được trước ngày hiện tại!");
         } else if (promotion.getNgayBatDau().isAfter(promotion.getNgayKetThuc())) {
@@ -87,12 +79,11 @@ public class DotGiamGiaController {
             model.addAttribute("startDate", promotion.getNgayBatDau());
             model.addAttribute("endDate", promotion.getNgayKetThuc());
         }
-         if (promotion.getNgayKetThuc() == null) {
+        if (promotion.getNgayKetThuc() == null) {
             isValid = true;
             model.addAttribute("errorNgayKetThuc", "Ngày kết thúc chưa được chọn!");
             model.addAttribute("startDate", promotion.getNgayBatDau());
-        }
-         else if (promotion.getNgayKetThuc().compareTo(promotion.getNgayBatDau()) == 0 || promotion.getNgayKetThuc().isBefore(promotion.getNgayBatDau())) {
+        } else if (promotion.getNgayKetThuc().compareTo(promotion.getNgayBatDau()) == 0 || promotion.getNgayKetThuc().isBefore(promotion.getNgayBatDau())) {
             isValid = true;
             model.addAttribute("errorNgayKetThuc", "Ngày kết thúc phải sau ngày bắt đầu!");
             model.addAttribute("startDate", promotion.getNgayBatDau());
@@ -118,31 +109,28 @@ public class DotGiamGiaController {
         if (promotion.getGiaTriGiam() == null) {
             isValid = true;
             model.addAttribute("errorGiaTriGiam", "Giá trị giảm chưa được nhập!");
-        }
-        else if (promotion.getGiaTriGiam() <= 5) {
+        } else if (promotion.getGiaTriGiam() <= 5) {
             isValid = true;
             model.addAttribute("startDate", promotion.getNgayBatDau());
             model.addAttribute("endDate", promotion.getNgayKetThuc());
             model.addAttribute("errorGiaTriGiam", "Giá trị giảm phải lớn hơn 5%!");
-        }
-        else if (promotion.getGiaTriGiam() > 50 && promotion.getLoaiGiamGia()) {
+        } else if (promotion.getGiaTriGiam() > 50 && promotion.getLoaiGiamGia()) {
             isValid = true;
             model.addAttribute("startDate", promotion.getNgayBatDau());
             model.addAttribute("endDate", promotion.getNgayKetThuc());
             model.addAttribute("errorGiaTriGiam", "Giá trị giảm phải nhỏ hơn 50%!");
+        } else if (promotion.getGiaTriGiam() < 5000 && !promotion.getLoaiGiamGia()) {
+            isValid = true;
+            model.addAttribute("startDate", promotion.getNgayBatDau());
+            model.addAttribute("endDate", promotion.getNgayKetThuc());
+            model.addAttribute("errorGiaTriGiam", "Giá trị giảm phải tối thiểu 5.000 vnđ!");
         }
-        else if (promotion.getGiaTriGiam() < 5000) {
-                isValid = true;
-                model.addAttribute("startDate", promotion.getNgayBatDau());
-                model.addAttribute("endDate", promotion.getNgayKetThuc());
-                model.addAttribute("errorGiaTriGiam", "Giá trị giảm phải tối thiểu 5.000 vnđ!");
-            }
-
 
         if (promotion.getGiaTriDonHang() == null) {
             isValid = true;
             model.addAttribute("errorBill", "Giá trị đơn hàng chưa được nhập!");
         }
+
 
         if (isValid) {
             return "admin/promotion/add-promotion";
